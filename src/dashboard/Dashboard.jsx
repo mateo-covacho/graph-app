cb0b58718b02acc35e7f7b077beae103caef9acd
Mateo Covacho Berrocal <mateocovachov2@gmail.com>
2022-05-25 15:38:25 +0200

    dark theme

---
+++ src/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Hello</h1>
+      <h2>Hellllllllllllllllllllllo</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
