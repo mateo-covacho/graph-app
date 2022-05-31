0de35529975859fd1fadf2750c5a2ab05a29acd8
Mateo Covacho Berrocal <mateocovachov2@gmail.com>
2022-05-31 18:40:32 +0200

    final graph demo

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -101,16 +101,8 @@ const Dashboard = (props) => {
                 size: 16,
               },
               physics: {
-                forceAtlas2Based: {
-                  gravitationalConstant: -26,
-                  centralGravity: 0.005,
-                  springLength: 230,
-                  springConstant: 0.18,
-                },
-                maxVelocity: 146,
-                solver: "forceAtlas2Based",
-                timestep: 0.35,
-                stabilization: { iterations: 150 },
+                stabilization: false,
+                wind: { x: 0, y: 0 },
               },
             }}
           >
