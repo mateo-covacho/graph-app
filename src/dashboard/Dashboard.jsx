c97545e916f72be9628d7498efc0300869fee742
Mateo Covacho Berrocal <mateocovachov2@gmail.com>
2022-06-01 13:45:08 +0200

    updated physics

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -14,21 +14,7 @@ import { Link } from "react-router-dom";
 const Dashboard = (props) => {
   const [toolBar, setToolBar] = useState("graph");
 
-  var options = {
-    physics: {
-      stabilization: false,
-    },
-    autoResize: true,
-    edges: {
-      smooth: true,
-      color: "#000000",
-      widt: 0.5,
-      arrows: {
-        enabled: true,
-        scaleFactor: 0.5,
-      },
-    },
-  };
+  const container = document.getElementsByClassName("toolbar");
 
   if (props.isAdmin) {
     return (
@@ -38,31 +24,31 @@ const Dashboard = (props) => {
             <Link to="/">
               <div className="row col-12 gx-0 ">
                 <AiOutlineHome className=" my-auto col-2 icon my-2  " />
-                <p className=" my-auto col align-bottom ps-3 ">Home</p>
+                <p className=" my-auto col align-bottom  ">Home</p>
               </div>
             </Link>
             <div className="row col-1 gx-0 ">
               <BiNetworkChart className="col-2 icon my-2  my-auto" />
-              <p className=" my-auto col align-bottom ps-3 ">Graph</p>
+              <p className=" my-auto col align-bottom  ">Graph</p>
             </div>
 
             <div className="row col-1 gx-0 ">
               <GrInstagram className="col-2 icon my-2 my-auto" />
-              <p className=" my-auto col align-bottom ps-3 ">Social</p>
+              <p className=" my-auto col align-bottom  ">Social</p>
             </div>
             <div className="row col-1 gx-0 ">
               <GiPathDistance className="col-2 icon my-2 my-auto" />
-              <p className=" my-auto col align-bottom ps-3 ">Algorithm</p>
+              <p className=" my-auto col align-bottom  ">Algorithm</p>
             </div>
 
             <div className="row col-3 gx-0 text-nowrap my-auto">
               <FaBitcoin className=" col-1 icon my-2 " />
-              <p className=" my-auto col align-bottom  text-nowrap ps-3 ">
+              <p className=" my-auto col align-bottom  text-nowrap  ">
                 Blockchain
               </p>
             </div>
           </div>
-          <div className="row g-0 toolbar">
+          <div className="row g-0 " id="toolbar">
             {(() => {
               switch (toolBar) {
                 case "graph":
@@ -98,11 +84,17 @@ const Dashboard = (props) => {
             options={{
               nodes: {
                 shape: "dot",
-                size: 16,
+                size: 30,
               },
               physics: {
-                stabilization: false,
-                wind: { x: 0, y: 0 },
+                barnesHut: {
+                  gravitationalConstant: -26100,
+                  centralGravity: 0,
+                  springLength: 240,
+                  springConstant: 0.025,
+                  avoidOverlap: 0.09,
+                },
+                minVelocity: 0.75,
               },
             }}
           >
